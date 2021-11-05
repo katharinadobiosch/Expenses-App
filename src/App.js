71e87cfa9c5096a897e8ab9e560415306e532c5f
Kathi <67746234+katharinadobiosch@users.noreply.github.com>
2021-11-05 15:38:51 +0100

    project finished

--- src/App.js
+++ src/App.js
@@ -5,26 +5,26 @@ import Expenses from "./components/Expenses/Expenses";
 const DUMMY_EXPENSES = [
     {
         id: "e1",
-        title: "Toilet Paper",
-        amount: 94.12,
+        title: "Beige Carpet",
+        amount: 399.95,
         date: new Date(2020, 7, 14),
     },
     {
         id: "e2",
-        title: "New beige TV",
-        amount: 799.9,
-        date: new Date(2021, 2, 12),
+        title: "Beige Parasol",
+        amount: 79.95,
+        date: new Date(2021, 7, 12),
     },
     {
         id: "e3",
-        title: "Beige Car Insurance",
-        amount: 294.67,
-        date: new Date(2021, 2, 28),
+        title: "Beige Blanket",
+        amount: 89.95,
+        date: new Date(2021, 7, 28),
     },
     {
         id: "e4",
-        title: "New beige Desk (Wooden)",
-        amount: 499.9,
+        title: "Beige Bikini",
+        amount: 119.95,
         date: new Date(2021, 5, 12),
     },
 ];
